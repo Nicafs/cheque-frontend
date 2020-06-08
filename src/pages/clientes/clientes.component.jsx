872ebcd8911a2cf59159e60c0ccdcd98a10ae301
Nicollas Santos <nicollas.comp@gmail.com>
2020-06-07 21:36:01 -0400

    Criando os filtros para as tabelas

--- src/pages/clientes/clientes.component.jsx
+++ src/pages/clientes/clientes.component.jsx
@@ -1,119 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import TableCustom from '../../core/components/table/tableCustom';
+import Filters from '../../core/components/filters/filters';
+import { find, filter } from '../../redux/client/client.actions';
 
-import { find } from '../../redux/client/client.actions';
+function Clientes({ findClients, data, filteredData, filterSubmit }) {
 
-const columns = [
-  {
-    Header: "Nome",
-    accessor: "name"
-  },
-  {
-    Header: "E-mail",
-    accessor: "email"
-  },
-  {
-    Header: "Data de Nascimento",
-    accessor: "birthDate"
-  },
-  {
-    Header: "Sexo",
-    accessor: "gender"
-  },
-  {
-    Header: "Celular",
-    accessor: "phone"
-  },
-  {
-    Header: "Endereço",
-    accessor: "address"
-  }
-];
+  const filters = [
+    { type: 'text', name: 'name', label: 'Nome', validators: 'requerid', value: null },
+    { type: 'date', name: 'birthDate', label: 'Data de Nascimento', validators: 'requerid', value: null },
+    { type: 'select', name: 'gender', label: 'Sexo', validators: 'requerid', value: null, selects: [{ description: 'Masculino', value: 'M' }, { description: 'Feminino', value: 'F' }] },
+    { type: 'text', name: 'phone', label: 'Telefone', validators: 'requerid', value: null, format: '(00) 00000-0000' },
+    { type: 'text', name: 'address', label: 'Endereço', validators: 'requerid', value: null }
+  ]
 
-class Clientes extends React.Component {
-  constructor({props}) {
-    super(props);
+  useEffect(() => {
+    findClients();
+  }, [findClients]);
+
+  const handleSubmit = (filtersSubmit) => {
+    filteredData = data;
+
+    filtersSubmit.map(filter => {
+      if (filter.value) {
+        filteredData = filteredData.filter(d =>  {
+          if(filter.type === 'text') return d[filter.name].includes(filter.value);
+          return d[filter.name] === filter.value;
+        })
+      }
+      return filteredData;
+    })
+
+    filterSubmit(filteredData);
   }
 
-  componentWillMount() {
-    console.log("Entrou no didmount, props:",this.props);
-    
-    const { dispatch } = this.props;
-    
-    dispatch(find());
-  }
-
-  render() {
-    const { data } = this.props;
-    return (
-      <div className="Clientes">
-        <div className="form">
-          <div className="row">
-            <div className="col-12 col-md-3">
-              <div className="form-group">
-                <label>Nome</label>
-                <Input type="text" className="form-control" id='nameForm'
-                  name="Name" value={this.state.user.Name}
-                  onChange={e => this.updateField(e)}
-                  placeholder="Digite o nome aqui..." />
-              </div>
-            </div>
-
-            <div className="col-12 col-md-6">
-              <div className="form-group">
-                <label>E-mail</label>
-                <Input type="text" className="form-control" id='emailForm'
-                  name="Description" value={this.state.user.Description}
-                  onChange={e => this.updateField(e)}
-                  placeholder="Digite othis. e-mail aqui..." />
-              </div>
-            </div>
-          </div>
-          <hr />
-          <div className="row">
-            <div className="col-12 d-flex justify-content-end">
-              <Button className="btn btn-secundarythis." 
-                onClick={() =>['/crud']}>
-                Criar
-                </Button>
-              <Button className="btn btn-primary  ml-2"
-                onClick={() =>  this.props.history.push('/clientes/crud')}>
-                Voltar
-              </Button>
-            </div>
-          </div>
-        </div>
-
-        <CssBaseline />
-        <EnhancedTable
-          columns={columns}
-          data={data}
-          setData={this.setData}
-          updateMyData={this.updateMyData}
-          skipPageReset={false}
-        />
+  return (
+    <div className="Clientes">
+      <div className="form">
+        <Filters filters={filters} handleSubmit={handleSubmit} />
       </div>
-    );
-  }
+      {filteredData ?
+        (<TableCustom data={filteredData} />)
+        : null}
+    </div >
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    data: state.client.data,
+    filteredData: state.client.filteredData
+  };
 }
 
+const mapDispatchToProps = dispatch => ({
+  filterSubmit: (data) => dispatch(filter(data)),
+  findClients: () => dispatch(find())
+});
 
-const mapStateToProps = (state) =>{
- return {
-    data : state.client.data
- };
-}
-const connectedClientsPage = withRouter(connect(mapStateToProps, null, null, {
- pure: false
-})(Clientes));
 
-export { connectedClientsPage as Clientes };
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Clientes);
